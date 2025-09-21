3db6570d0186c79aa3d20769b6d63a01a7c8eb60
Gabryel <st_gabriel@hotmail.fr>
2025-09-22 00:02:42 +0200

    Update actor.mjs

---
+++ module/documents/actor.mjs
@@ -0,0 +1,14 @@
+/**
+ * La classe de base pour tous les acteurs du système Guildes.
+ */
+export class GuildesActor extends Actor {
+
+  /**
+   * Préparation des données dérivées.
+   * C'est ici que nous pourrons plus tard faire les calculs des Arts.
+   */
+  prepareDerivedData() {
+    super.prepareDerivedData();
+  }
+
+}
